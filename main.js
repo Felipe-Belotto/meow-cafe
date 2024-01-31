e3f6bcac1578c78392072f9153632be20f78a1ca
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-01-31 12:34:36 -0300

    adicionando funcionalidade de fixar o menu lateral no topo / corrigida

--- main.js
+++ main.js
@@ -2,30 +2,10 @@ const menuHamburguer = document.querySelector("#menu-hamburguer");
 const menuContainer = document.querySelector(".menu-hamburguer-elements");
 const root = document.querySelector("#root");
 
-let startY = 0;
+window.addEventListener("scroll", () => {
 
-menuContainer.addEventListener("touchstart", (event) => {
-  startY = event.touches[0].clientY;
-});
-
-menuContainer.addEventListener("touchmove", (event) => {
-  const currentY = event.touches[0].clientY;
-  const deltaY = currentY - startY;
-
-  if (deltaY > 0) {
-    menuHamburguer.checked = false;
-  }
-});
-
-menuContainer.addEventListener("mousedown", (event) => {
-  startY = event.clientY;
-});
-
-menuContainer.addEventListener("mousemove", (event) => {
-  const currentY = event.clientY;
-  const deltaY = currentY - startY;
-
-  if (deltaY > 0) {
-    menuHamburguer.checked = false;
+  if (menuHamburguer.checked) {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
   }
 });
