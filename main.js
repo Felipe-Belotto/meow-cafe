0fe824629460ded1472898a2a6f815b6094790c7
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-01-31 12:31:33 -0300

    Função de guardar o menu lateral

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+const menuHamburguer = document.querySelector("#menu-hamburguer");
+const menuContainer = document.querySelector(".menu-hamburguer-elements");
+const root = document.querySelector("#root");
 
-const menuHamburguer = document.getElementById("menu-hamburguer")
+let startY = 0;
 
+menuContainer.addEventListener("touchstart", (event) => {
+  startY = event.touches[0].clientY;
+});
 
-menuHamburguer.addEventListener("click", ()=>{
- //crie uma função que bloqueie o scroll da página enquanto estiver ativo
- document.body.classList.toggle("active")
- 
+menuContainer.addEventListener("touchmove", (event) => {
+  const currentY = event.touches[0].clientY;
+  const deltaY = currentY - startY;
 
-})
+  if (deltaY > 0) {
+    menuHamburguer.checked = false;
+  }
+});
+
+menuContainer.addEventListener("mousedown", (event) => {
+  startY = event.clientY;
+});
+
+menuContainer.addEventListener("mousemove", (event) => {
+  const currentY = event.clientY;
+  const deltaY = currentY - startY;
+
+  if (deltaY > 0) {
+    menuHamburguer.checked = false;
+  }
+});
